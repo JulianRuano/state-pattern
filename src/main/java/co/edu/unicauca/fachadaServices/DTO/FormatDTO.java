fd247b6844f928e359269051f2612230d96158a5
JulianRuano <julianrmajin@gmail.com>
2025-03-06 15:03:40 -0500

    feat: agregar soporte para subtipos en FormatDTO utilizando JsonTypeInfo y JsonSubTypes

--- src/main/java/co/edu/unicauca/fachadaServices/DTO/FormatDTO.java
+++ src/main/java/co/edu/unicauca/fachadaServices/DTO/FormatDTO.java
@@ -2,6 +2,11 @@ package co.edu.unicauca.fachadaServices.DTO;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+import co.edu.unicauca.fachadaServices.DTO.request.FormatPPDTORequest;
+import co.edu.unicauca.fachadaServices.DTO.request.FormatTIDTORequest;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,6 +15,15 @@ import lombok.Setter;
 @Getter@Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonTypeInfo(
+    use = JsonTypeInfo.Id.NAME,
+    include = JsonTypeInfo.As.PROPERTY,
+    property = "type"
+)
+@JsonSubTypes({
+    @JsonSubTypes.Type(value = FormatPPDTORequest.class, name = "PP"),
+    @JsonSubTypes.Type(value = FormatTIDTORequest.class, name = "TI")
+})
 public class FormatDTO {
     private Integer id;
     private String title;
