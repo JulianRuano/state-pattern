1d4e75cf6147e5a7d56b72d7b610075145ead9bb
JulianRuano <julianrmajin@gmail.com>
2025-03-07 14:52:35 -0500

    refactor: eliminar mapeos innecesarios en FormatMapper y simplificar la conversión entre entidades y DTOs

--- src/main/java/co/edu/unicauca/fachadaServices/mapper/FormatMapper.java
+++ src/main/java/co/edu/unicauca/fachadaServices/mapper/FormatMapper.java
@@ -20,17 +20,10 @@ public interface FormatMapper {
     @SubclassMapping(source = FormatTIEntity.class, target = FormatTIDTOResponse.class)
     FormatDTOResponse toFormatDTOResponse(FormatEntity formatEntity);
 
-
-    @SubclassMapping(source = FormatPPDTOResponse.class, target = FormatPPEntity.class)
-    @SubclassMapping(source = FormatTIDTOResponse.class, target = FormatTIEntity.class)
-    FormatEntity toFormatEntity(FormatDTOResponse formatDTO);
-
-
     @SubclassMapping(source = FormatPPEntity.class, target = FormatPPDTORequest.class)
     @SubclassMapping(source = FormatTIEntity.class, target = FormatTIDTORequest.class)
     FormatDTORequest toFormatDTORequest(FormatEntity formatEntity);
     
-
     @SubclassMapping(source = FormatPPDTORequest.class, target = FormatPPEntity.class)
     @SubclassMapping(source = FormatTIDTORequest.class, target = FormatTIEntity.class)
     FormatEntity toFormatEntity(FormatDTORequest formatDTO);
